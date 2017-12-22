2434f296c71ff601560309b35c76f57bbb46de51
Rudrasish <rudrapanda117@gmail.com>
2017-12-22 11:19:14 +0530

    rendering array of courses

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-courses',
-    template: '<h2> {{ title }} </h2>'
+    template: `
+    <h2> {{ title }} </h2>
+    <ul>
+    <li *ngFor="let course of courses">{{ course }}</li>
+    </ul>
+    `
 })
 export class CoursesComponent {
-    // title = 'List of courses';
+     title = 'List of courses';
+    courses = ['course1', 'course2', 'course3'];
 
-    // getTitle() {
-    //     return this.title;
-    // }
-    get title() {
-        return 'List of courses';
-    }
 }
