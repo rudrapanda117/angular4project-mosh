5560b7039a3ef23a995412354ec7232980cc73bb
Rudrasish <rudrapanda117@gmail.com>
2017-12-22 12:40:53 +0530

    Attribute binding vs property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,11 +1,15 @@
-import { CoursesService } from './courses.service';
-import { Component } from '@angular/core';
+import {
+  CoursesService
+} from './courses.service';
+import {
+  Component
+} from '@angular/core';
 
 
 
 @Component({
-    selector: 'app-courses',
-    template: `
+  selector: 'app-courses',
+  template: `
     <h2> {{ title }} </h2>
     <ul>
     <li *ngFor="let course of courses">{{ course }}</li>
@@ -14,17 +18,24 @@ import { Component } from '@angular/core';
     <img src="{{ imageUrl }}"/>
     <h2 [textContent]="propertyBinding"></h2>
     <img [src]="imageUrl" />
+
+    <table>
+        <tr>
+            <td [attr.colspan]="colSpan" ></td>
+        </tr>
+    </table>
     `
 })
 export class CoursesComponent {
-    propertyBinding = ' Displaying image by Property Binding';
-    stringInterPolation = 'Displaying image by String Interpolation';
-        title = 'List of courses';
-    imageUrl = 'http://lorempixel.com/400/200';
-    courses;
+  propertyBinding = ' Displaying image by Property Binding';
+  stringInterPolation = 'Displaying image by String Interpolation';
+  title = 'List of courses';
+  imageUrl = 'http://lorempixel.com/400/200';
+  courses;
+  colspan = 2;
 
-    constructor(private coursesService: CoursesService) {
-        this.courses = coursesService.getCourses();
-    }
+  constructor(private coursesService: CoursesService) {
+    this.courses = coursesService.getCourses();
+  }
 
 }
