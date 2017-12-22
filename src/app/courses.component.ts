5f738790ae6bb73b218805be4c60778ea427300d
Rudrasish <rudrapanda117@gmail.com>
2017-12-22 14:12:41 +0530

    Two way binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,90 +10,24 @@ import {
 @Component({
   selector: 'app-courses',
   template: `
-    <h2> {{ title }} </h2>
-    <ul>
-    <li *ngFor="let course of courses">{{ course }}</li>
-    </ul>
-    <h2 >{{ stringInterPolation }}</h2>
-    <img src="{{ imageUrl }}"/>
-    <h2 [textContent]="propertyBinding"></h2>
-    <img [src]="imageUrl" />
+    <!-- two way binding -->
+    <input [value]="email" (keyup.enter)="email = $event.target.value; ontwbEnter()" />
 
-    <table>
-        <tr>
-            <td [attr.colspan]="colSpan" ></td>
-        </tr>
-    </table>
-    <!-- event bubling -->
-    <div (click)="onSecondDivClicked()">
-    <div (click)="onDivClicked($event);">
-    <button class="btn btn-primary" [style.backgroundColor]="isActive? 'blue' : 'red' " (click)="onSave($event)">Save</button>
-    </div>
-    </div>
-
-    <!-- event filtering -->
-    <input (keyup)="onKeyUp($event)"/>
-
-    <input (keyup.enter)="onKeyUpEnter()"/>
-
-    <!-- template variable -->
-    <input (keyup.enter)="onKeyUpEnterValue($event)"/>
-
-    <input #templateVariable (keyup.enter)="onKeyUpEnterTemplateVariable(templateVariable.value)"/>
+    <input [(ngModel)]="email" (keyup.enter)="ontwbEnter()" />
 
     `
 })
 export class CoursesComponent {
-  propertyBinding = ' Displaying image by Property Binding';
-  stringInterPolation = 'Displaying image by String Interpolation';
-  title = 'List of courses';
-  imageUrl = 'http://lorempixel.com/400/200';
-  courses;
-  colspan = 2;
-  isActive = true;
-  onSave($event) {
-    console.log('button was clicked', $event);
-  }
 
-  onDivClicked($event) {
-    // $event.stopPropagation();
-    console.log('First Div clicked');
-  }
-
-  onSecondDivClicked() {
-    console.log('Second Div clicked');
-  }
-
-  onKeyUp($event) {
-    if ($event.keyCode === 13) {
-      console.log('Enter was pressed');
-    }
-
-  }
-
-  onKeyUpEnter() {
-
-      console.log('Enter was pressed');
+  email = 'a@b.com';
 
 
-  }
-
-  onKeyUpEnterValue($event) {
-
-    console.log('Enter was pressed', $event.target.value);
-
-
-}
-
-onKeyUpEnterTemplateVariable(value) {
-
-    console.log('Enter was pressed', value);
-
-
+ontwbEnter() {
+    console.log('email value', this.email);
 }
 
   constructor(private coursesService: CoursesService) {
-    this.courses = coursesService.getCourses();
+
   }
 
 }
