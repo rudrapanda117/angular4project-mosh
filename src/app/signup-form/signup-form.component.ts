1ad973f63d272377961629d5b6edfb10235e34cf
Rudrasish <rudrapanda117@gmail.com>
2017-12-23 23:17:25 +0530

    Async Validation

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,6 +1,14 @@
-import { UsernameValidators } from './username.validator';
-import { Component } from '@angular/core';
-import { FormGroup , FormControl, Validators } from '@angular/forms';
+import {
+  UsernameValidators
+} from './username.validator';
+import {
+  Component
+} from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
@@ -9,14 +17,19 @@ import { FormGroup , FormControl, Validators } from '@angular/forms';
 })
 export class SignupFormComponent {
 
-form = new FormGroup({
-  'username': new FormControl('', [Validators.required, Validators.minLength(3), UsernameValidators.cannotContainsSpace]),
-  'password': new FormControl('', Validators.required)
-});
-get username() {
-  return this.form.get('username');
-}
+  form = new FormGroup({
+    'username': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidators.cannotContainsSpace,
+      UsernameValidators.shouldBeUnique
+    ]),
+    'password': new FormControl('', Validators.required)
+  }, {
+    updateOn: 'blur'
+  });
+  get username() {
+    return this.form.get('username');
+  }
 
 }
-
-
--- src/app/signup-form/username.validator.ts
+++ src/app/signup-form/username.validator.ts
@@ -1,11 +1,31 @@
-import { ValidationErrors, AbstractControl } from '@angular/forms';
+import {
+  ValidationErrors,
+  AbstractControl
+} from '@angular/forms';
 export class UsernameValidators {
-   static cannotContainsSpace(control: AbstractControl): ValidationErrors | null {
+  static cannotContainsSpace(control: AbstractControl): ValidationErrors | null {
 
-        if ((control.value as string).indexOf(' ') !== -1) {
-            return {
-                cannotContainsSpace: true
-            };
-        }
+    if ((control.value as string).indexOf(' ') !== -1) {
+        console.log("cannotContainsSpace");
+      return {
+        cannotContainsSpace: true
+      };
     }
+  }
+
+  static shouldBeUnique(control: AbstractControl): Promise < ValidationErrors | null > {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "mosh") {
+            console.log("shouldBeUnique true");
+          resolve({
+            shouldBeUnique: true
+          });
+        } else {
+            console.log("shouldBeUnique false");
+          resolve(null);
+        }
+      }, 2000);
+    });
+  }
 }
