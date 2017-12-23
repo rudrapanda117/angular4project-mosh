de7b6e144a636aca5a9f54e18d828d3703059283
Rudrasish <rudrapanda117@gmail.com>
2017-12-23 18:15:41 +0530

    Custom drective

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,5 @@ import {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [{
-      id: 1,
-      name: 'course1 '
-    },
-    {
-      id: 2,
-      name: 'course2 '
-    },
-    {
-      id: 3,
-      name: 'course3 '
-    }
-  ];
-
-  onAdd() {
-    this.courses.push({id: 4, name: 'course4 '});
-  }
-
-  onRemove(course) {
-    const index = this.courses.indexOf(course);
-    this.courses.splice(index, 1);
-  }
-
-  onUpdate(course) {
-    course.name = 'updated';
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AuthorsService } from './authors.service';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.component';
 import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { LikeComponent } from './like/like.component';
     SumamryPipe,
     FavoriteComponent,
     BootstrapPanelComponent,
-    LikeComponent
+    LikeComponent,
+    InputFormatDirective
   ],
   imports: [
     BrowserModule,
