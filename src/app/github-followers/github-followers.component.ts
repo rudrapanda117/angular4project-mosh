e62d42a1b4dca30601fecaf5447c6936021e2a5a
Rudrasish <rudrapanda117@gmail.com>
2017-12-24 21:33:48 +0530

    Query Params

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubFollowersService } from './github-followers.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  GithubFollowersService
+} from './github-followers.service';
+import {
+  ActivatedRoute
+} from '@angular/router';
+import {
+  log
+} from 'util';
 
 @Component({
   selector: 'app-github-followers',
@@ -8,11 +19,25 @@ import { GithubFollowersService } from './github-followers.service';
 })
 export class GithubFollowersComponent implements OnInit {
   public followers: any[];
-  constructor(private service: GithubFollowersService) { }
+  constructor(private service: GithubFollowersService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+
+    this.route.paramMap
+      .subscribe(params => {
+        console.log('pathParams', params);
+      });
+
+    this.route.queryParamMap
+      .subscribe(params => {
+        console.log('query Params', params);
+        let page = +params.get('page');
+        let order = params.get('order');
+        console.log('page: ', page, ' order : ', order);
+      });
+
     this.service.getAll()
-    .subscribe(followers => this.followers = followers );
+      .subscribe(followers => this.followers = followers);
   }
 
 }
