0e5f0436ee7b99d62427cec2ac0d00bad6a51149
Rudrasish <rudrapanda117@gmail.com>
2017-12-24 13:30:58 +0530

    Update data patch method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -37,4 +37,12 @@ export class PostsComponent implements OnInit {
     });
   }
 
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+    .subscribe(response => {
+      console.log(response.json());
+    });
+    //this.http.put(this.url, JSON.stringify(post));
+  }
+
 }
