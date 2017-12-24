75fd3ddaba430b18a805d995fd6463582863b0ab
Rudrasish <rudrapanda117@gmail.com>
2017-12-24 20:48:49 +0530

    Addin router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,96 @@
-import { AppErrorHandler } from './common/app-error-handler';
-import { ErrorHandler } from '@angular/core';
-import { PostService } from './posts/post.service';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { SumamryPipe } from './summary.pipe';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {
+  GithubProfileService
+} from './github-profile/github-profile.service';
+import {
+  NotFoundComponent
+} from './not-found/not-found.component';
+import {
+  GithubProfileComponent
+} from './github-profile/github-profile.component';
+import {
+  HomeComponent
+} from './home/home.component';
+import {
+  NavbarComponent
+} from './navbar/navbar.component';
+import {
+  AppErrorHandler
+} from './common/app-error-handler';
+import {
+  ErrorHandler
+} from '@angular/core';
+import {
+  PostService
+} from './posts/post.service';
+import {
+  SignupFormComponent
+} from './signup-form/signup-form.component';
+import {
+  SumamryPipe
+} from './summary.pipe';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule
+} from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import {
+  HttpModule
+} from '@angular/http';
+import {
+  RouterModule
+} from '@angular/router';
 
 
-import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
-import { CourseComponent } from './course/course.component';
-import { CoursesService } from './courses.service';
-import { AuthorsComponent } from './authors/authors.component';
-import { AuthorsService } from './authors.service';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.component';
-import { LikeComponent } from './like/like.component';
-import { InputFormatDirective } from './input-format.directive';
-import { ZippyComponent } from './zippy/zippy.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { PostsComponent } from './posts/posts.component';
-import { GithubFollowersComponent } from './github-followers/github-followers.component';
-import { GithubFollowersService } from './github-followers/github-followers.service';
+import {
+  AppComponent
+} from './app.component';
+import {
+  CoursesComponent
+} from './courses.component';
+import {
+  CourseComponent
+} from './course/course.component';
+import {
+  CoursesService
+} from './courses.service';
+import {
+  AuthorsComponent
+} from './authors/authors.component';
+import {
+  AuthorsService
+} from './authors.service';
+import {
+  FavoriteComponent
+} from './favorite/favorite.component';
+import {
+  BootstrapPanelComponent
+} from './bootstrap-panel/bootstrap-panel.component';
+import {
+  LikeComponent
+} from './like/like.component';
+import {
+  InputFormatDirective
+} from './input-format.directive';
+import {
+  ZippyComponent
+} from './zippy/zippy.component';
+import {
+  ContactFormComponent
+} from './contact-form/contact-form.component';
+import {
+  PostsComponent
+} from './posts/posts.component';
+import {
+  GithubFollowersComponent
+} from './github-followers/github-followers.component';
+import {
+  GithubFollowersService
+} from './github-followers/github-followers.service';
 
 
 @NgModule({
@@ -41,23 +108,52 @@ import { GithubFollowersService } from './github-followers/github-followers.serv
     ContactFormComponent,
     SignupFormComponent,
     PostsComponent,
-    GithubFollowersComponent
-    ],
+    GithubFollowersComponent,
+    NavbarComponent,
+    HomeComponent,
+    GithubProfileComponent,
+    NotFoundComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'followers/:username',
+        component: GithubProfileComponent
+      },
+      {
+        path: 'followers',
+        component: GithubFollowersComponent
+      },
+      {
+        path: 'posts',
+        component: PostsComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      }
+
+    ])
   ],
   providers: [
     CoursesService,
     AuthorsService,
     PostService,
     GithubFollowersService,
+    GithubProfileService,
     {
-      provide: ErrorHandler, useClass: AppErrorHandler
+      provide: ErrorHandler,
+      useClass: AppErrorHandler
     }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
